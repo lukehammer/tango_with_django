c19173e92c8f3d76cfafe8b847d43c6bbcf48e58
luke hammer <luke@lukehammermagic.com>
2014-11-20 19:29:18 -0800

    moving to labtop midday

--- tango_with_django/tango_with_django/settings.py
+++ tango_with_django/tango_with_django/settings.py
@@ -70,17 +70,20 @@ MEDIA_URL = ''
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
 
+STATIC_PATH = os.path.join(PROJECT_PATH ,'static')
+
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    STATIC_PATH
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-
+print STATIC_PATH
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
